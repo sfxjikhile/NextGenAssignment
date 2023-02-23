7d964dbfbf266a11be8ddb10ca178d4df654c4be
chuks <nwajeigoddowell@gmail.com>
2023-02-23 12:06:17 +0100

    second commit

--- src/main/java/com/assignments/questions/Calc.java
+++ src/main/java/com/assignments/questions/Calc.java
@@ -1,5 +1,6 @@
 package com.assignments.questions;
 
+
 public class Calc {
     public static void main(String[] args){
         int number = 4 * (4 + 8) / 4;
--- src/main/java/com/assignments/questions/Salary.java
+++ src/main/java/com/assignments/questions/Salary.java
@@ -2,6 +2,7 @@ package com.assignments.questions;
 
 import java.util.Scanner;
 
+
 public class Salary {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
