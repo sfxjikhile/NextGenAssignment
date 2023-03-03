ff85b99c4a0d931070cccdaeb84acc4f06fa41fd
chuks <nwajeigoddowell@gmail.com>
2023-03-03 21:28:44 +0100

    second commit

---
+++ src/main/java/com/assignments/questions/DecrementingLoop.java
@@ -0,0 +1,11 @@
+package com.assignments.questions;
+
+public class DecrementingLoop {
+
+    public static void main(String[] args){
+        for(int i = 1000; i >= 0; i-= 2) {
+            System.out.println(i);
+
+        }
+    }
+}
