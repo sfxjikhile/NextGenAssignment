1b1e3e9bc38463dde87d9ec7e64c9ae14d0b0665
chuks <nwajeigoddowell@gmail.com>
2023-03-03 15:59:09 +0100

    first commit

---
+++ src/main/java/com/assignments/questions/DisplayMenu.java
@@ -0,0 +1,51 @@
+package com.assignments.questions;
+
+import java.util.Scanner;
+
+public class DisplayMenu {
+    String response = "";
+
+    String enterData() {
+      response = "Enter Data";
+      return response;
+    }
+
+    String displayData() {
+        response = "Display Data";
+        return response;
+    }
+
+    String exitMenu() {
+        response = "Exit";
+        return response;
+    }
+
+
+    public static void main(String [] args){
+        DisplayMenu displayMenu = new DisplayMenu();
+
+        // Displayed Menu
+        System.out.println("1. Enter Data \n2. Display Data \n3. Exit\n");
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Input Menu Option:");
+
+        // User input
+        int userInput = sc.nextInt();
+
+        switch(userInput) {
+            case 1:
+                System.out.println(displayMenu.enterData());
+                break;
+            case 2:
+                System.out.println(displayMenu.displayData());
+                break;
+            case 3:
+                System.out.println(displayMenu.exitMenu());
+                break;
+            default:
+        }
+    }
+
+}
---
+++ src/main/java/com/assignments/questions/Table.java
@@ -0,0 +1,10 @@
+package com.assignments.questions;
+
+public class Table {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 12; i++) {
+            System.out.print("7 * " + i + " = ");
+            System.out.println(i * 5);
+        }
+    }
+}
