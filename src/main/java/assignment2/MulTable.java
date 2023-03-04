de0537e822be39a6aea13d19652df936e5d1c030
timstag <towolabi151@gmail.com>
2023-03-04 18:27:49 +0100

    Owolabi Timileyin assignment2

---
+++ src/main/java/assignment2/MulTable.java
@@ -0,0 +1,10 @@
+package assignment2;
+
+  public class MulTable {
+    public static void main(String[] args) {
+     int num = 5;
+       for (int x = 0; x <= 12; x++) {
+         System.out.println(num + " * " + x + " = " + num * x);
+}
+    }
+}
---
+++ src/main/java/assignment2/NumProgression.java
@@ -0,0 +1,11 @@
+package assignment2;
+
+  public class NumProgression {
+    public static void main(String[] args) {
+
+      for(int x = 1; x <= 1000; x = x * 2) {
+
+        System.out.println(x);
+}
+    }
+}
