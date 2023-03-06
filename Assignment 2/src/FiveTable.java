096292593f41870417720a9a50f142e2a77dc659
Greg005 <122963589+Greg005@users.noreply.github.com>
2023-03-06 09:58:28 +0100

    Add files via upload

---
+++ Assignment 2/src/FiveTable.java
@@ -0,0 +1,9 @@
+public class FiveTable {
+    public static void main(String[] args) {
+        int num = 5;
+        // Number whose table needs to be printed
+        for (int i = 1; i <=12; i++) {
+            System.out.println(num + " x " + i + " = " + (num*i));
+        }
+    }
+}
