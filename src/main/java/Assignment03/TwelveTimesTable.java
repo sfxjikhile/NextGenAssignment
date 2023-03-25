dc6fbac4b0798dbbfb4c4ebc775dca480236a8e9
Emmanuel Osademe <emma.osademe@gmail.com>
2023-03-25 09:59:43 +0100

    Revisited TwelveTimesTable based on feedback from instructor

--- src/main/java/Assignment03/TwelveTimesTable.java
+++ src/main/java/Assignment03/TwelveTimesTable.java
@@ -1,15 +1,25 @@
 package Assignment03;
 
+import java.util.Arrays;
+
 public class TwelveTimesTable {
     public static void main(String[] args){
-        int[] resultArray = new int[12];
+        int[][] resultArray = new int[12][12];
 
-        for(int i = 1; i <= resultArray.length; i++){
-            resultArray[i - 1] = i * 12;
+        for(int i = 1; i <= resultArray[1].length; i++){
+            int[] newArray = new int[12];
+            for(int j = 1; j <= newArray.length; j++){
+                newArray[j -1] = j * i;
+            }
+            //System.out.println(Arrays.toString(newArray));
+            resultArray[i -1] = newArray;
         }
 
-        for(int i = 0; i < resultArray.length; i++){
-            System.out.println("12 x " + (i + 1) + ": " +resultArray[i]);
+        /*
+            Could have printed the array within the former for-block. But decided to bring it out to add make things clearer.
+         */
+        for(int[] subArray: resultArray){
+            System.out.println(Arrays.toString(subArray));
         }
     }
 }
