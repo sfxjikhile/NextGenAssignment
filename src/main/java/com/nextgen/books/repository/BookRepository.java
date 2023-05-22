4a27b2cdda8bbd68d796cf92908d1667e012b8d6
Janet Ikhile <jikhile@seamfix.com>
2023-05-22 12:02:17 +0100

    Spring Security implemented

--- src/main/java/com/nextgen/books/repository/BookRepository.java
+++ src/main/java/com/nextgen/books/repository/BookRepository.java
@@ -4,7 +4,9 @@ import com.nextgen.books.entity.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface BookRepository extends JpaRepository<Book, Integer> {
 
     Book findBookById(int id);
--- src/main/java/com/nextgen/books/service/UserService.java
+++ src/main/java/com/nextgen/books/service/UserService.java
@@ -4,15 +4,18 @@ import com.nextgen.books.config.AppProperties;
 import com.nextgen.books.dto.UserRegistrationRequest;
 import com.nextgen.books.dto.UserRegistrationResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class UserService {
     private final AppProperties appProperties;
 
     public UserRegistrationResponse registerUser(UserRegistrationRequest userRegistrationRequest) {
         String email = userRegistrationRequest.getEmail();
+        log.debug("The request from client is {}", userRegistrationRequest);
         UserRegistrationResponse userRegistrationResponse = new UserRegistrationResponse(1, "You cannot sign up with email; sign up with username instead");
         if (appProperties.isEnableEmailSignUp()) {
             userRegistrationResponse.setCodeAndDescription(1, "You can sign up with email");
